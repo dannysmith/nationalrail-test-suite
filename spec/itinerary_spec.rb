0b0a0fd2e35ccaddbe7892113b853335d01f5594
Jane Last <jane.kempton@gmail.com>
2014-10-15 16:58:57 +0100

    Got everything up to closing the print box to work

--- spec/itinerary_spec.rb
+++ spec/itinerary_spec.rb
@@ -1,23 +1,36 @@
-require 'watir-webdriver'
+require 'watir'
 require 'spec_helper'
+require 'date'
 
-
+FROM = "Richmond (London)"
+TO   = "Sutton (Surrey)"
+FILE = DateTime.now.strftime("%d%b%Y%H%M%S")
 describe "Create and print an Itinerary on NR" do
+  before(:all) do
   open_browser
-  
+  end
   # To choose any UK location
   it "Should be able to choose any UK location" do
-    @b.text_field(:id, "txtFrom").set "Richmond"
+    enter_destinations FROM, TO
+    expect(@b.text.include? "Richmond (London)")
+    expect(@b.text.include? "Sutton (Surrey)")
   end
   
   # To create a journey itinerary
   it "Should be able to create a journey itinerary" do
-    
+    confirm_journey
+    @b.link(:id, "journeyOption0").click
+    expect(@b.text.include? "Route details")
   end
   
   # To print itinerary
   it "Should be able to print itinerary" do
-    
+    @b.link(:href, "javascript:window.print()").click
+    sleep(2)
+    @b.screenshot.save("./screenshots/#{FILE}.png")
+    @b.window(:title, "Print").use do
+      b.close
+    end
   end
   
   
