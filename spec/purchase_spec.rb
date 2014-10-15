12d45c9e613b2fb6ce60ead0f7e3c2a00106598d
Aaron Muir <aaron.muir21@gmail.com>
2014-10-15 14:58:46 +0100

    Fixed station variables

--- spec/purchase_spec.rb
+++ spec/purchase_spec.rb
@@ -1,19 +1,24 @@
 require 'spec_helper'
 require 'watir'
 
+FROM = "RMD"
+TO = "SUO"
+
 describe "Buying a return ticket on NR" do
 	before(:all) do
 		  open_browser
 	end
 
 	it "Can search for select journies" do
-		@b.text_field(:id, "txtFrom").set "Richmond"
-		@b.text_field(:id, "txtTo").set "Sutton"	
+		enter_destinations FROM, TO
+		confirm_journey
+	end
+
+	it "" do
 
 	end
 
-
 	after(:all) do
-		close_browser
+		#close_browser
 	end
 end
