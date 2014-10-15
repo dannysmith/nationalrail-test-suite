fdfa1a5a4c853a8ae327802bc3e6897b3463e935
Aaron Muir <aaron.muir21@gmail.com>
2014-10-15 16:39:50 +0100

    Purchase spec added second test

--- spec/purchase_spec.rb
+++ spec/purchase_spec.rb
@@ -9,16 +9,21 @@ describe "Buying a return ticket on NR" do
 		  open_browser
 	end
 
-	it "Can search for select journies" do
+	it "Can search for select journeys" do
 		enter_destinations FROM, TO
+		@b.checkbox(:name, "checkbox").set
+
 		confirm_journey
+
+		expect(@b.text.include? "Richmond (London) [RMD] to Sutton (Surrey) [SUO]")
 	end
 
-	it "" do
-
+	it "You can buy a return ticket from these destinations" do
+		@b.button(:id, "buyCheapestButton").click
+		expect(@b.url.include? "northernrail")
 	end
 
 	after(:all) do
-		#close_browser
+		close_browser
 	end
 end
