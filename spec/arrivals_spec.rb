b6ba732f0c7bbbbf5e62b9b7c33031d435203f0b
Trishul Gohil <trishulgohil@live.co.uk>
2014-10-16 13:22:50 +0100

    Live arrivals/ departures tests completed

--- spec/arrivals_spec.rb
+++ spec/arrivals_spec.rb
@@ -9,27 +9,81 @@ describe "Live Arrival/Departures Tests" do
     after :each do
         close_browser
     end
-    
-    it "should allow you to search for any valid uk station" do
+	
+	# Test 1 - Search for multiple stations. Sercing for 3 stations, after this it is assumed that it will work for all UK stations.
+	it "Allow searches to different UK stations" do
+		
+        place1 = "Richmond (London)"
+		place2 = "Glasgow Central"
+		place3 = "Cardiff Central"
+		
+        @b.a(:title, "Train times & tickets").click
+        @b.a(:title, "Live departure boards").click
+		
+        @b.text_field(:id, "train-from").click
+        @b.text_field(:id, "train-from").clear
+        @b.text_field(:id, "train-from").send_keys(place1)
+        @b.button(:id, "planJourney").click
+        @b.h1(:class, "sifr").wait_until_present
+
+        expect(@b.div.li(:class, "departing active").text.include?("Departing")).to eq(true)
+        expect(@b.h2.text.include?(place1)).to eq(true)
+			
+			
+        @b.a(:title, "Live departure boards").click
+		
+        @b.text_field(:id, "train-from").click
+        @b.text_field(:id, "train-from").clear
+        @b.text_field(:id, "train-from").send_keys(place2)
+        @b.button(:id, "planJourney").click
+        @b.h1(:class, "sifr").wait_until_present
+		expect(@b.h2.text.include?(place2)).to eq(true)
+		
+		@b.a(:title, "Live departure boards").click
+		
+        @b.text_field(:id, "train-from").click
+        @b.text_field(:id, "train-from").clear
+        @b.text_field(:id, "train-from").send_keys(place3)
+        @b.button(:id, "planJourney").click
+        @b.h1(:class, "sifr").wait_until_present
+		expect(@b.h2.text.include?(place3)).to eq(true)
+    end
+	
+	# Test 2 - Departures
+	it "Show departures for Richmond station" do
         place = "Richmond (London)"
         @b.a(:title, "Train times & tickets").click
         @b.a(:title, "Live departure boards").click
+		
+        @b.text_field(:id, "train-from").click
+        @b.text_field(:id, "train-from").clear
+        @b.text_field(:id, "train-from").send_keys(place)
+        @b.button(:id, "planJourney").click
+        @b.h1(:class, "sifr").wait_until_present
+
+        expect(@b.div.li(:class, "departing active").text.include?("Departing")).to eq(true)
+        
+        expect(@b.h2.text.include?(place)).to eq(true)
+        
+    end
+    
+    # Test 3 - Arrivals
+    it "Show arrivals for Richmond station" do
+        place = "Richmond (London)"
+        @b.a(:title, "Train times & tickets").click
+        @b.a(:title, "Live departure boards").click
+        @b.a(:class, "ldb-r ldb-arrivals").click
+        
         @b.text_field(:id, "train-from").click
         @b.text_field(:id, "train-from").clear
         @b.text_field(:id, "train-from").send_keys(place)
         @b.button(:id, "planJourney").click
         @b.h1(:class, "sifr").wait_until_present
         
-#        puts "SUCCESS" if @b.h1(:class, "sifr").text.include? "yufiuffuifif"
         
-        
-        fail unless @b.h1(:class, "sifr").text.include? "Live departures & arrivals"
-        
-        expect(@b.h1.text.include?("Live departures & arrivals")).to eq(true)
+        expect(@b.div.li(:class, "arriving active").text.include?("Arriving")).to eq(true)
         
         expect(@b.h2.text.include?(place)).to eq(true)
-        
-#        puts "Richmond departures page shown!" if @b.span(:class, "to").text.include?(place)
         
         
     end
