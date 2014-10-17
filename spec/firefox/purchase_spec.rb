e95edf7989a787118edd3000fe0d675d014ef2ea
Aaron Muir <aaron.muir21@gmail.com>
2014-10-17 12:02:58 +0100

    fixed purchase spec

--- spec/firefox/purchase_spec.rb
+++ spec/firefox/purchase_spec.rb
@@ -31,10 +31,9 @@ describe "Buying a return ticket on NR" do
   end
 
   it "Will throw an error when time in selected before current" do
-	@b.goto "nationalrail.co.uk"
+	return_homepage
 	enter_destinations FROM, TO
-	today_date
-	@b.text_field(:id, 'txtDate').set date
+	@b.text_field(:id, 'txtDate').set "17/10/2014"
 	@b.select_list(:id, 'sltHours').select_value("01")
 	confirm_journey
 
